30ab78f76b253d876a4728efb791cbf4b850823f
David Quigley <UCSF>
2015-10-28 10:59:09 -0700

    version 0.1

--- drug_response/server.R
+++ drug_response/server.R
@@ -7,18 +7,18 @@ fit_model = function( dd, concentrations, conditions ){
     conc = rep(concentrations, dim(dd)[2] )
     response=as.numeric(matrix(data.matrix(dd), nrow=dim(dd)[1] * dim(dd)[2], ncol=1))
     dd.long = data.frame( response=response, conc=conc  )
-    curveids = c()
+    curveids = rep(0, length(dd.long))
+    N = dim(dd)[1]
     for(i in 1:length(conditions)){
-        curveids = c( curveids, rep( conditions[i], dim(dd)[1] ) )
+        curveids[ (  ((i-1)*N)+1 ) : (i*N) ] = conditions[i]
     }
-    curveids = factor(curveids)
-    drm( dd.long, curveid=curveids, fct = LL2.2() )
+    drm( response~conc, data=dd.long, curveid=curveids, fct = LL2.2() )
 }
 
 extract_sf = function( model, y_val ){
     y_val = 0.5
-    b_fitted = coef(m1)[ 1 : (length(coef(m1))/2) ]
-    e_fitted = coef(m1)[ ((length(coef(m1))/2)+1) : length(coef(m1)) ]
+    b_fitted = coef(model)[ 1 : (length(coef(model))/2) ]
+    e_fitted = coef(model)[ ((length(coef(model))/2)+1) : length(coef(model)) ]
     y_fitted = rep(0, length(b_fitted))
     for( i in 1:length(b_fitted)){
         y_fitted[i] = ((1/y_val)-1)/exp(b_fitted[i]) + exp(e_fitted[i])
@@ -26,15 +26,22 @@ extract_sf = function( model, y_val ){
     y_fitted
 }
 
-plot.dose.response = function( dd, concentrations, conditions, xlab, ylab, show50=TRUE ){
+plot.dose.response = function( dd, concentrations, conditions, xlab, ylab, show50=TRUE, xmax=5, ymax=1 ){
     
     m1 = fit_model( dd, concentrations, conditions )
-    plot(m1, type="none", lty=c(1,1), pch=c(15,15),  xlim=c(0,1e5), ylim=c(0,1), 
-         axes=FALSE, legend=FALSE, cex.lab=1.25, col=c("black", "red"),
-         xlab=xlab, ylab=ylab, lwd=2 )
-    axis(2, at=c(0, 0.5, 1), labels=c("0", "", "1"), las=1)
-    axis(1, at=c(1e1, 1e2, 1e3, 1e4, 1e5), labels=c(1,2,3,4,5), las=1)
-    unique.conditions = unique(conditions)
+    par(mar=c(6,6,2,2))
+    N = length(unique(conditions))
+    plot(m1, type="none", lty=rep(1, N), pch=rep(15, N),  xlim=c(0, (10^xmax)), ylim=c(0,ymax), 
+         axes=FALSE, legend=FALSE, cex.lab=2, font.lab=2, col=colors[1:N],
+         xlab=xlab, ylab=ylab, lwd=2, yaxs="i", xaxs="i" )
+    box(col="white", lwd=3)
+    axis(2, at=c(0, 0.5, 1), labels=c("0", "", "1"), las=1, font.axis=2, cex.axis=1.5)
+    xlabel = c()
+    for( i in 1:xmax ){
+        xlabel = c( xlabel, paste("1e", i, sep="") )
+    }
+    axis(1, at=xlabel, labels=1:xmax, las=1, font.axis=2, font.lab=2, cex.axis=1.5)
+    unique.conditions = unique(names(dd))
     for(i in 1:length(unique.conditions) ){
         points( concentrations, rowMeans(dd[,conditions==unique.conditions[i]]), pch=15, col=colors[i], cex=1.2)
     }
@@ -64,16 +71,18 @@ se=function(x){
     sd(x, na.rm=T)/sqrt(sum(!is.na(x))) 
 }
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
     
     output$drugresponsePlot <- renderPlot({
-        if( !is.null( input$file$datapath) ){
+        if( !is.null( input$file$datapath ) ){
             dd = load.matrix(input$file$datapath[1])
             concentrations =  as.numeric(rownames(dd))
             conditions = factor(names(dd))
-            legends = sort(unique(names(dd)))
-            plot.dose.response( dd, concentrations, conditions, input$xlab, input$ylab, input$sf50 )
-            legend( 10, 0.1, legends, pch=15, cex=1.25, col=colors[1:length(legends)], bty="n")
+            legends = unique(names(dd))
+            plot.dose.response( dd, concentrations, conditions, input$xlab, input$ylab, input$sf50, as.numeric(input$xmax), as.numeric(input$ymax))
+            legend( as.numeric(input$legend_x), as.numeric(input$legend_y), legends, pch=15, cex=1.25, col=colors[1:length(legends)], bty="n")
+            
+            session$sendCustomMessage("download_PDF", list())
         }
     })
     
@@ -88,4 +97,21 @@ shinyServer(function(input, output) {
             data = data.frame( legends, SF50=y_fitted, stringsAsFactors=FALSE )
         }
     })
+    
+    output$download_PDF <- downloadHandler(
+        filename = function() {
+            "dose_response.pdf"
+        },
+        
+        content = function(file) {
+            pdf( file, width=10, height=8 )
+            dd = load.matrix(input$file$datapath[1])
+            concentrations =  as.numeric(rownames(dd))
+            conditions = factor(names(dd))
+            legends = sort(unique(names(dd)))
+            plot.dose.response( dd, concentrations, conditions, input$xlab, input$ylab, input$sf50, as.numeric(input$xmax) )
+            legend( as.numeric(input$legend_x), as.numeric(input$legend_y), legends, pch=15, cex=1.25, col=colors[1:length(legends)], bty="n")
+            dev.off()
+        }
+    )
 })
--- drug_response/ui.R
+++ drug_response/ui.R
@@ -3,22 +3,41 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
     
+    singleton(tags$head(HTML(
+        '
+        <script type="text/javascript">
+        $(document).ready(function() {
+        $("#download_PDF").attr("disabled", "true").attr("onclick", "return false;");
+        
+        Shiny.addCustomMessageHandler("download_PDF", function(message) {
+        $("#download_PDF").removeAttr("disabled").removeAttr("onclick").html(
+        "<i class=\\"fa fa-download\\"></i>Download PDF");
+        });
+        })
+        </script>
+        '
+    ))),
+    
     # Application title
     titlePanel("Drug response curve"),
     
     # Sidebar with a slider input for the number of bins
     sidebarLayout(
         sidebarPanel(
-        #    sliderInput("bins",
-        #                "Number of bins:",
-        #                min = 1,
-        #                max = 50,
-        #                value = 30)
-        
-        fileInput("file", label = "File" ),
-        textInput("xlab", label = "X axis label", value = "Log drug concentration"),
-        textInput("ylab", label = "Y axis label", value = "viability"),
-        checkboxInput("sf50", label = "Show SF50", value = TRUE)
+        fileInput("file", label = "File",width="200px" ),
+        textInput("xlab", label = "X axis label", value = "Log drug concentration", width="150px"),
+        textInput("ylab", label = "Y axis label", value = "viability", width="150px"),
+        selectInput("xmax", label = "Maximum X value", 
+                    choices = list("1e1" = 1, "1e2" = 2,"1e3" = 3, "1e4" = 4, 
+                                   "1e5" = 5, "1e6" = 6, "1e7" = 7), 
+                    selected = 5, width="150px"),
+        selectInput("ymax", label = "Maximum Y value", 
+                    choices = list("1" = 1, "1.1" = 1.1,"1.2" = 1.2), 
+                    selected = 1, width="150px"),
+        checkboxInput("sf50", label = "Show SF50", value = TRUE),
+        textInput("legend_x", label = "Legend X location", value = "10", width="150px"),
+        textInput("legend_y", label = "Legend Y location", value = "0.2", width="150px"),
+        downloadButton("download_PDF")
         ),
         # Show a plot of the generated distribution
         mainPanel(
